4aa67c5102bd75ca8ccaacb8192fc94f484c2717
traitimtrongvag <pan2512811@gmail.com>
2026-01-01 23:25:04 +0700

    Solve Java Subarray

---
+++ Introduction/Java Subarray/JavaSubarray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+        int count = 0;
+        
+        for (int start = 0; start < n; start++) {
+            int sum = 0;
+            for (int end = start; end < n; end++) {
+                sum += arr[end];
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+        
+        System.out.println(count);
+        scanner.close();
+    }
+}
