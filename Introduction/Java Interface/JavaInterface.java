3509aa40c2454895421b79647b4c890f756f4a2c
traitimtrongvag <pan2512811@gmail.com>
2026-01-12 22:37:24 +0700

    Solve Java Interface

---
+++ Introduction/Java Interface/JavaInterface.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+interface AdvancedArithmetic {
+    int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        if (n == 1) return 1;
+        
+        int sum = 1 + n;
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 2; i <= sqrt; i++) {
+            if (n % i == 0) {
+                sum += i;
+                if (i != n / i) {
+                    sum += n / i;
+                }
+            }
+        }
+        return sum;
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+        sc.close();
+    }
+    
+    static void ImplementedInterfaceNames(Object o) {
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++) {
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
